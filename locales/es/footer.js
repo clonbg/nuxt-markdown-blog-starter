4c3167c7daa8432f2bb7ed68632abb70303e5d9f
clonbg <clonbg@gmail.com>
2020-03-10 10:52:00 +0100

    Eliminando archivos en inglés

--- locales/es/footer.js
+++ locales/es/footer.js
@@ -1,3 +1,3 @@
 export default {
-  credits: 'Open source template. Mira el código en <a target="_blank" href="https://github.com/marinaaisa/nuxt-markdown-blog-starter" class="ani"><span>Github</span></a>. Hecho por <a target="_blank" href="https://marinaaisa.com/es/" class="ani"><span>Marina Aisa</span></a>.'
+  credits: 'clonbg <a target="_blank" href="https://github.com/clonbg?tab=repositories" class="ani"><span>Github</span></a> <br>Open source template. Mira el código hecho por <a target="_blank" href="https://marinaaisa.com/es/" class="ani"><span>Marina Aisa</span></a> en <a target="_blank" href="https://github.com/marinaaisa/nuxt-markdown-blog-starter" class="ani"><span>Github</span></a>.'
 }
